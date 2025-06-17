232c09f9298e728b3c8c3b43b973016312d30753
Rodney Miley <rodney.miley1@engineering.digital.dwp.gov.uk>
2025-06-17 07:12:38 +0100

    initial commit

--- app/filters.js
+++ app/filters.js
@@ -6,5 +6,7 @@
 const govukPrototypeKit = require('govuk-prototype-kit')
 const addFilter = govukPrototypeKit.views.addFilter
 
-// Add your filters here
+
+
+
 
--- app/routes.js
+++ app/routes.js
@@ -7,3 +7,12 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
+router.post('/scope-answer', function(request, response) {
+
+    var scope = request.session.data['scope']
+    if (scope == "yes"){
+        response.redirect("/v1/tasklist")
+    } else {
+        response.redirect("/v1/tasklist")
+    }
+})
