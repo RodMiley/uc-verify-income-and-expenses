815d97737b41a47fd93dee8a65b4a8ac2259f14b
Rodney Miley <rodney.miley1@engineering.digital.dwp.gov.uk>
2025-06-20 11:55:46 +0100

    Added moment
    Added date filter and updated content

--- app/filters.js
+++ app/filters.js
@@ -5,6 +5,25 @@
 
 const govukPrototypeKit = require('govuk-prototype-kit')
 const addFilter = govukPrototypeKit.views.addFilter
+var moment = require('moment')
+
+
+
+
+addFilter('baseDate', function (format,num) {
+    var num = Math.ceil(num);
+      var d = moment().subtract(num,"days").format(format)
+      if (d !== 'Invalid date') return d
+      else return ''
+})
+
+
+addFilter('baseDateAdd', function (format,num) {
+var num = Math.ceil(num);
+    var d = moment().add(num,"days").format(format)
+    if (d !== 'Invalid date') return d
+    else return ''
+})
 
 
 
